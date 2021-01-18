93c5a0f3c799c798d7331c44e771c7284f757eb4
Christian Briones <cwbriones@gmail.com>
2021-01-18 15:23:02 -0800

    implement rotations via unit quaternions

--- src/geom.rs
+++ src/geom.rs
@@ -19,7 +19,7 @@ pub struct Point3(f64, f64, f64);
 
 impl Point3 {
     #[inline]
-    pub fn at(x: f64, y: f64, z: f64) -> Self {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
         Point3(x, y, z)
     }
 
@@ -44,6 +44,21 @@ impl Point3 {
     #[inline]
     pub fn get(&self, i: usize) -> f64 {
         [self.0, self.1, self.2][i]
+    }
+
+    #[inline]
+    pub fn x(&self) -> f64 {
+        self.0
+    }
+
+    #[inline]
+    pub fn y(&self) -> f64 {
+        self.1
+    }
+
+    #[inline]
+    pub fn z(&self) -> f64 {
+        self.2
     }
 }
 
@@ -85,7 +100,7 @@ impl Vec3 {
     }
 
     #[inline]
-    pub fn zhat() -> Self {
+    pub fn khat() -> Self {
         Vec3::new(0.0, 0.0, 1.0)
     }
 
@@ -154,6 +169,13 @@ impl Vec3 {
     // #[inline]
     pub fn get(&self, i: usize) -> f64 {
         [self.0, self.1, self.2][i]
+    }
+
+    #[inline]
+    pub fn rel_eq(&self, other: &Self, epsilon: f64) -> bool {
+        (self.0 - other.0).abs() < epsilon
+            && (self.1 - other.1).abs() < epsilon
+            && (self.2 - other.2).abs() < epsilon
     }
 }
 
@@ -271,5 +293,207 @@ impl DivAssign<f64> for Vec3 {
         self.0 /= c;
         self.1 /= c;
         self.2 /= c;
+    }
+}
+
+/// A unit-length quarternion, which can be used for rotations.
+#[derive(Debug, Clone, Copy)]
+pub struct UnitQuaternion {
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
+}
+
+impl UnitQuaternion {
+    /// Create a new unit quarternion from the given components.
+    pub fn new(x: f64, y: f64, z: f64, w: f64) -> Self {
+        let inv_mag = (x * x + y * y + z * z + w * w).sqrt().recip();
+        UnitQuaternion {
+            x: inv_mag * x,
+            y: inv_mag * y,
+            z: inv_mag * z,
+            w: inv_mag * w,
+        }
+    }
+
+    /// A unit quaternion oriented along the x-axis.
+    #[allow(unused)]
+    pub const fn i() -> Self {
+        UnitQuaternion {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+            w: 0.0,
+        }
+    }
+
+    /// A unit quaternion oriented along the y-axis.
+    #[allow(unused)]
+    pub const fn j() -> Self {
+        UnitQuaternion {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+            w: 0.0,
+        }
+    }
+
+    /// A unit quaternion oriented along the z-axis.
+    #[allow(unused)]
+    pub const fn k() -> Self {
+        UnitQuaternion {
+            x: 0.0,
+            y: 0.0,
+            z: 1.0,
+            w: 0.0,
+        }
+    }
+
+    /// Return the conjugate of this quaternion.
+    #[inline]
+    pub fn conj(&self) -> Self {
+        UnitQuaternion {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+            w: self.w,
+        }
+    }
+
+    /// Create a quaternion from a desired rotation around an axis.
+    #[allow(unused)]
+    pub fn rotation(axis: Vec3, angle: f64) -> Self {
+        let cos = (angle / 2.0).cos();
+        let sin = (angle / 2.0).sin();
+
+        Self::new(sin * axis.x(), sin * axis.y(), sin * axis.z(), cos)
+    }
+
+    /// Rotate a vector using this quaternion.
+    #[allow(unused)]
+    pub fn rotate_vec(&self, direction: Vec3) -> Vec3 {
+        let lhs = UnitQuaternion {
+            x: direction.x(),
+            y: direction.y(),
+            z: direction.z(),
+            w: 0.0,
+        } * self.conj();
+        let out = *self * lhs;
+
+        Vec3::new(out.x, out.y, out.z)
+    }
+
+    /// Rotate a point using this quaternion.
+    #[allow(unused)]
+    pub fn rotate_point(&self, direction: Point3) -> Point3 {
+        let lhs = UnitQuaternion {
+            x: direction.x(),
+            y: direction.y(),
+            z: direction.z(),
+            w: 0.0,
+        } * *self;
+        let out = self.conj() * lhs;
+
+        Point3::new(out.x, out.y, out.z)
+    }
+
+    #[cfg(test)]
+    #[inline]
+    pub fn rel_eq(&self, other: &Self, epsilon: f64) -> bool {
+        (self.x - other.x).abs() < epsilon
+            && (self.y - other.y).abs() < epsilon
+            && (self.z - other.z).abs() < epsilon
+            && (self.w - other.w).abs() < epsilon
+    }
+}
+
+impl Mul<UnitQuaternion> for UnitQuaternion {
+    type Output = UnitQuaternion;
+
+    fn mul(self, other: UnitQuaternion) -> UnitQuaternion {
+        // See: http://ariya.blogspot.com/2010/07/faster-quaternion-multiplication.html
+
+        let ww = (self.z + self.x) * (other.x + other.y);
+        let yy = (self.w - self.y) * (other.w + other.z);
+        let zz = (self.w + self.y) * (other.w - other.z);
+        let xx = ww + yy + zz;
+        let qq = 0.5 * (xx + (self.z - self.x) * (other.x - other.y));
+
+        let w = qq - ww + (self.z - self.y) * (other.y - other.z);
+        let x = qq - xx + (self.x + self.w) * (other.x + other.w);
+        let y = qq - yy + (self.w - self.x) * (other.y + other.z);
+        let z = qq - zz + (self.z + self.y) * (other.w - other.x);
+
+        UnitQuaternion { x, y, z, w }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EPS: f64 = 1E-8;
+
+    #[test]
+    fn quaternion_multiplication_table() {
+        let i = UnitQuaternion::i();
+        let j = UnitQuaternion::j();
+        let k = UnitQuaternion::k();
+
+        let ii = i * i;
+        let jj = j * j;
+        let kk = k * k;
+
+        let ij = i * j;
+        let jk = j * k;
+        let ki = k * i;
+
+        let ijk = i * jk;
+
+        let one = UnitQuaternion::new(0.0, 0.0, 0.0, 1.0);
+        let neg_one = UnitQuaternion::new(0.0, 0.0, 0.0, -1.0);
+
+        assert!(ii.rel_eq(&neg_one, EPS), "ii = {:?}", ii);
+        assert!(jj.rel_eq(&neg_one, EPS), "jj = {:?}", jj);
+        assert!(kk.rel_eq(&neg_one, EPS), "kk = {:?}", kk);
+
+        assert!(ij.rel_eq(&k, EPS), "ij = {:?}", ij);
+        assert!(jk.rel_eq(&i, EPS), "jk = {:?}", jk);
+        assert!(ki.rel_eq(&j, EPS), "ki = {:?}", ki);
+
+        assert!(ijk.rel_eq(&neg_one, EPS), "ijk = {:?}", ijk);
+        assert!((one * one).rel_eq(&one, EPS), "1 * 1 = {:?}", one * one);
+    }
+
+    #[test]
+    fn quaternion_rotation() {
+        let rotate_around_z = UnitQuaternion::rotation(Vec3::khat(), ::std::f64::consts::FRAC_PI_2);
+        let ihat = Vec3::ihat();
+
+        let rotated = rotate_around_z.rotate_vec(ihat);
+        assert!(
+            rotated.rel_eq(&Vec3::jhat(), EPS),
+            "i-hat rotates 90deg around z-hat to j-hat"
+        );
+    }
+
+    #[test]
+    fn quaternion_rotation_composition() {
+        let rotate_around_z = UnitQuaternion::rotation(Vec3::khat(), ::std::f64::consts::FRAC_PI_2);
+        let rotate_around_x = UnitQuaternion::rotation(Vec3::ihat(), ::std::f64::consts::FRAC_PI_2);
+
+        // Rotate around z, then around x.
+        // around z: i-hat -> j-hat
+        // around x: j-hat -> k-hat
+        let full_rotation = rotate_around_x * rotate_around_z;
+
+        let ihat = Vec3::ihat();
+        let rotated = full_rotation.rotate_vec(ihat);
+        assert!(
+            rotated.rel_eq(&Vec3::khat(), EPS),
+            "Expected k-hat, got {:?}",
+            rotated
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -150,7 +150,7 @@ fn random_scene<R: Rng>(mut rng: R) -> Scene {
 
     let ground_material = Material::lambertian(Vec3::new(0.5, 0.5, 0.5));
     objects.push(Sphere::new(
-        Point3::at(0., -1000., 0.),
+        Point3::new(0., -1000., 0.),
         1000.,
         ground_material,
     ));
@@ -158,12 +158,12 @@ fn random_scene<R: Rng>(mut rng: R) -> Scene {
     for a in -11..11 {
         for b in -11..11 {
             let choose_material = rng.gen::<f64>();
-            let center = Point3::at(
+            let center = Point3::new(
                 a as f64 + 0.9 * rng.gen::<f64>(),
                 0.2,
                 b as f64 + 0.9 * rng.gen::<f64>(),
             );
-            if (center - Point3::at(4.0, 0.2, 0.0)).length() > 0.9 {
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
                 let material: Material;
                 if choose_material < 0.8 {
                     // diffuse
@@ -183,11 +183,11 @@ fn random_scene<R: Rng>(mut rng: R) -> Scene {
         }
     }
     let material1 = Material::lambertian(Vec3::new(0.05, 0.2, 0.6));
-    objects.push(Sphere::new(Point3::at(-4., 1., 0.), 1.0, material1));
+    objects.push(Sphere::new(Point3::new(-4., 1., 0.), 1.0, material1));
     let material2 = Material::dielectric(1.5);
-    objects.push(Sphere::new(Point3::at(0., 1., 0.), 1.0, material2));
+    objects.push(Sphere::new(Point3::new(0., 1., 0.), 1.0, material2));
     let material3 = Material::metal(Vec3::new(0.7, 0.6, 0.5), 0.0);
-    objects.push(Sphere::new(Point3::at(4., 1., 0.), 1.0, material3));
+    objects.push(Sphere::new(Point3::new(4., 1., 0.), 1.0, material3));
 
     Scene::new(objects)
 }
@@ -227,8 +227,8 @@ fn main() {
             let mut rng = small_rng(config.seed);
             s.spawn(move |_| {
                 let camera = Camera::builder(20.0, ASPECT_RATIO)
-                    .from(Point3::at(13., 2., 3.))
-                    .towards(Point3::at(0., 0., 0.))
+                    .from(Point3::new(13., 2., 3.))
+                    .towards(Point3::new(0., 0., 0.))
                     .focus_dist(10.0)
                     .aperture(0.1)
                     .build();

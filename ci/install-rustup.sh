27d19db53bcb6225d9c628271d0275895fd6e22a
Christian Briones <cwbriones@gmail.com>
2021-02-13 12:11:44 -0800

    non-interactive

--- ci/install-rustup.sh
+++ ci/install-rustup.sh
@@ -2,8 +2,9 @@
 
 if [[ ! -e ~/.rustup ]]; then
     echo "Installing rustup..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/install-rustup.sh
+    sh /tmp/install-rustup.sh -y
 else
-    echo "Rustup installation found. Updating."
+    echo "rustup installation found. Updating."
     rustup self update
 fi

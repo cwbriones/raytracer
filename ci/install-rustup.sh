9f22052167f20f9f689047967652b26ad1fb63f4
Christian Briones <cwbriones@gmail.com>
2021-02-13 12:26:51 -0800

    forwarding the first rustup call seems weird

--- ci/install-rustup.sh
+++ ci/install-rustup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 if [[ ! -e ~/.rustup ]]; then
-    echo "Installing rustup..."
+    echo "rustup installation not found, running rustup-init..."
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/install-rustup.sh
     sh /tmp/install-rustup.sh -y
-elif [[ ! -e /tmp/rustup-init ]]; then
-    echo "rustup installation found. Updating."
+elif [[ ! -e /tmp/rustup-updated ]]; then
+    echo "rustup installation found. updating."
     rustup self update
 fi
 
-touch /tmp/rustup-init
-
-rustup $@
+touch /tmp/rustup-updated
